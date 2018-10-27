a96fbac113e8642e47f12f826ba0cd01ba0a73ee
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-27 15:14:10 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,38 @@
+﻿const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+console.log('Oynuyor Oynuyor!.');
+console.log('Bot hazır.');
+
+    var Games = [
+
+        "%yardım Aktif!!!!!!!",
+    
+        "NABER?",
+        
+        `${client.users.size} Kullanıcı Adamsınız`,
+        
+        `${client.guilds.size} Sunucu Adamsınız`,
+        
+        `Beni Eklermisin %davet`,
+
+
+
+        `Yapımcım RK|BebeMustafa`
+
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+
+        client.user.setGame(Games[random], "twitch.tv/tunamustafa");
+        }, 2 * 2500);
+
+};
